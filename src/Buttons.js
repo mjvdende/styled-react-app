d646dc904f579ca9af2731a28f1d3890474daf09
Ruben Oostinga <roostinga@xebia.com>
2016-10-28 10:55:40 +0200

    Add bootstrap buttons

---
+++ src/Buttons.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: inline-block;
+  padding: 6px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.42857143;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  -ms-touch-action: manipulation;
+  touch-action: manipulation;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  background-image: none;
+  border: 1px solid transparent;
+  border-radius: 4px;
+`;
+
+export const DefaultButton = styled(Button)`
+  color: #333;
+  background-color: #fff;
+  border-color: #ccc;
+`;
+
+export const PrimaryButton = styled(Button)`
+  color: #fff;
+  background-color: #337ab7;
+  border-color: #2e6da4;
+`;
+
+export const SuccessButton = styled(Button)`
+  color: #fff;
+  background-color: #5cb85c;
+  border-color: #4cae4c;
+`;
+
+export const InfoButton = styled(Button)`
+  color: #fff;
+  background-color: #5bc0de;
+  border-color: #46b8da;
+`;
+
+export const WarningButton = styled(Button)`
+  color: #fff;
+  background-color: #f0ad4e;
+  border-color: #eea236;
+`;
+
+export const DangerButton = styled(Button)`
+  color: #fff;
+  background-color: #d9534f;
+  border-color: #d43f3a;
+`;
