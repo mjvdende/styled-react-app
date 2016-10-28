0b79977751b53d075ee3ad917056f94a9c6e8172
Norbert de Langen <ndelangen@me.com>
2016-10-28 14:23:43 +0200

    ADD unit tests to storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const config = {
+  module: {
+    loaders: [
+      {
+        test: /\.css?$/,
+        loaders: ['style', 'raw'],
+        include: path.resolve(__dirname, '../'),
+      },
+      {
+        test: /\.json?$/,
+        loaders: ['json'],
+        include: path.resolve(__dirname, '../'),
+      },
+      {
+        test: /\.svg?$/,
+        loaders: ['file'],
+        include: path.resolve(__dirname, '../'),
+      }
+    ],
+  },
+  externals: {
+    'jsdom': 'window',
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': 'window',
+    'react/addons': true,
+  }
+};
+
+module.exports = config;
--- src/App.story.js
+++ src/App.story.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
+// import { storiesOf, action, linkTo } from '@kadira/storybook';
+
+import { mount } from "enzyme";
+// import { specs, describe, it } from 'storybook-addon-specifications'
+import expect from 'expect';
+
+import {
+  storiesOf,
+  action,
+  describe,
+  it,
+  specs,
+  beforeEach,
+  before,
+  after,
+  afterEach,
+  xdescribe,
+  fit,
+  xit
+} from "../.storybook/facade";
+
 import App from './App';
 
 storiesOf('App', module)
-  .add('Default', () => (
-    <App />
-  ));
+  .add('Default', () => {
+    const story = <App />;
+
+    specs(() => describe('Default', () => {
+      it('Should have the Hello World label', () => {
+        let output = mount(story);
+        expect(output.text()).toContain('Welcome');
+      });
+    }));
+
+    return story;
+  });
