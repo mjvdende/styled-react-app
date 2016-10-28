ec417c3aa7bc8d5086dada9e5c7bad75c6ab3e4d
Jurriaan Mous <jurmous@jurmo.us>
2016-10-28 13:16:51 +0200

    import setAddon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
-import { configure } from '@kadira/storybook';
+import { configure, setAddon } from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
 
 setAddon(infoAddon);
