6c7ab2de62b99b67daf89e78a645a30866f892cb
Norbert de Langen <ndelangen@me.com>
2016-10-28 09:51:53 +0200

    INITIAL commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import logo from './logo.svg';
+import './App.css';
+
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+const Header = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+const Logo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+`;
+const Intro = styled.p`
+  font-size: large;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Header>
+          <Logo src={logo} alt="logo" />
+          <h2>Welcome to React</h2>
+        </Header>
+        <Intro>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </Intro>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
